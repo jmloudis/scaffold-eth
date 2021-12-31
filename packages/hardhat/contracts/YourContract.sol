33a868bbee4d6530bd3c83cbd97132b8bd890829
jordan <16jloudis@gmail.com>
2021-12-30 22:45:20 -0500

    Added new changes to the contract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -11,21 +11,51 @@ contract YourContract {
 
   string public purpose = "Building Unstoppable Apps!!!";
 
-  address public owner;
+  address payable public owner;
+  
 
   constructor() {
     // what should we do on deploy?
-    owner = msg.sender;
+    owner = payable(msg.sender);
   }
 
-  // function withdrawel() public {
-  //   require(msg.sender == owner);
-  //   msg.sender.transfer((address(this)).balance);
-  // }
+  receive() external payable {}
+
+
+    // Function to deposit Ether into this contract.
+    // Call this function along with some Ether.
+    // The balance of this contract will be automatically updated.
+  function deposit() public payable {
+      console.log(msg.sender, "Just deposited", msg.value);
+      
+  }
+
+
+    // Call this function along with some Ether.
+    // The function will throw an error since this function is not payable.
+  function withdrawel(uint _withdrawelAmount) public{
+
+  
+    console.log(msg.sender, "Just sent", (address(this)), _withdrawelAmount);
+    
+    payable(msg.sender).transfer(address(this).balance);
+
+  }
+
+
+
+
 
   function setPurpose(string memory newPurpose) public {
       purpose = newPurpose;
       console.log(msg.sender,"set purpose to",purpose);
       // emit SetPurpose(msg.sender, purpose);
   }
+
+  //  function transfer(address payable _to, uint _amount) public {
+  //       // Note that "to" is declared as payable
+  //       (bool success, ) = _to.call{value: _amount}("");
+  //       require(success, "Failed to send Ether");
+  //   }
+
 }
